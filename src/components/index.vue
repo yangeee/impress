23ed7e2b977797e2a14f3c7ef0071729e85f56cc
simpleyang <simpleyang@wondershare.cn>
2021-09-28 10:11:47 +0800

    【更新】：背景

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-  import { fabric } from 'fabric'
-  import { onMounted, reactive } from 'vue';
+  import {fabric} from 'fabric'
+  import {onMounted, reactive} from 'vue';
   import json from '../canvas.json'
 
   export default {
@@ -30,7 +30,7 @@
         })
         return rect
       }
-      const addText =  (value, item) =>{
+      const addText = (value, item) => {
         let text = new fabric.Textbox(value, {
           left: item.left, // 左上角位置
           top: item.top, // 左上角位置
@@ -41,26 +41,28 @@
         return text
       }
       let items = json.scenes[0].elements
-      function canvasInit() {
+
+      async function canvasInit() {
         canvas = new fabric.Canvas('canvas')
+        await canvasBgInit(canvas)
         fabric.Object.prototype.transparentCorners = false
         items.forEach(item => {
           switch (item.render) {
             case 'Image':
-            fabric.Image.fromURL(item.url, (img, err) => {
-              if(!err) {
-                img.set({
-                  left: item.data.left, // 左上角位置
-                  top: item.data.top, // 左上角位置
-                  width: item.data.width,
-                  height: item.data.height,
-                  target: false,
-                  crossOrigin: 'anonymous' // 使用的图片跨域时，配置此参数，有时会失效
-                })
-                canvas.add(img)
-              }
-            })
-            break
+              fabric.Image.fromURL(item.url, (img, err) => {
+                if (!err) {
+                  img.set({
+                    left: item.data.left, // 左上角位置
+                    top: item.data.top, // 左上角位置
+                    width: item.data.width,
+                    height: item.data.height,
+                    target: false,
+                    crossOrigin: 'anonymous' // 使用的图片跨域时，配置此参数，有时会失效
+                  })
+                  canvas.add(img)
+                }
+              })
+              break
             case 'Text':
               canvas.add(addText(item.text, item.data))
               console.log(canvas)
@@ -71,45 +73,64 @@
               break
           }
         })
-        canvas.on({'mouse:down': function(e) {
-          findObject(e.pointer.x, e.pointer.y)
-        }})
+        canvas.on({
+          'mouse:down': function (e) {
+            findObject(e.pointer.x, e.pointer.y)
+          }
+        })
       }
-      onMounted(() => {   
+
+      function canvasBgInit(canvas) {
+        return new Promise((res)=>{
+          const img = new Image()
+          img.src = json.background
+          img.onload = function () {
+            console.log(1)
+            let imgInstance = new fabric.Image(img, {
+              left: 0,
+              top: 0,
+            })
+            canvas.add(imgInstance)
+            res()
+          }
+        })
+      }
+
+      onMounted(() => {
         canvasInit()
       })
-      
+
       const findObject = (x, y) => {
+        console.log(11111)
         canvas._objects.forEach(element => {
-          if(element.left < x && element.left + element.width > x && element.top < y && element.top + element.height > y ) {
+          if (element.left < x && element.left + element.width > x && element.top < y && element.top + element.height > y) {
             canvas._objects.forEach(element => {
-              element.animate({opacity: 0} , {
-              duration: 200,
-              easing: fabric.util.ease.easeOutCubic,
-              onChange: canvas.renderAndResetBound,
+              element.animate({opacity: 0}, {
+                duration: 200,
+                easing: fabric.util.ease.easeOutCubic,
+                onChange: canvas.renderAndResetBound,
+              })
             })
-          })
-          var rect2 = new fabric.Rect({
-            left: 60,
-            top: 10,
-            width: 100,
-            height: 100,
-            fill: 'green',
-            angle: 45,
-            padding: 10,
-            stroke: 'green', // 边框颜色
-            strokeWidth: 4, // 边框宽度
-          });
-          setTimeout(() => {
-            canvas.clear()
-            var canvas1 = new fabric.Canvas('canvas');
-            canvas1.add(rect2)
-          },200)          
-        }
-      })
+            var rect2 = new fabric.Rect({
+              left: 300,
+              top: 200,
+              width: 200,
+              height: 200,
+              fill: 'green',
+              angle: 45,
+              padding: 10,
+              stroke: 'black', // 边框颜色
+              strokeWidth: 6, // 边框宽度
+            });
+            setTimeout(() => {
+              canvas.clear()
+              var canvas1 = new fabric.Canvas('canvas');
+              canvas1.add(rect2)
+            }, 200)
+          }
+        })
       }
-      
-      
+
       return {
         data
       }
